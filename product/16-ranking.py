0ca3ba87af78546f3ed1b5343bf274c50a75ceaa
nardtree <gim.kobayashi@gmail.com>
2018-01-24 14:49:25 +0900

    アップデート

--- product/16-ranking.py
+++ product/16-ranking.py
@@ -19,9 +19,9 @@ for term, target in term_vec.items():
   term_rel[ term ] = d/b
 
 terms = []
-for term, rel in sorted(term_rel.items(), key=lambda x:x[1]*-1)[0:200]:
+for term, rel in sorted(term_rel.items(), key=lambda x:x[1]*-1)[0:150]:
   # print(term, rel)
-  if 'iphone' not in term:
+  if 'ipad' not in term and '6s' not in term and 'plus' not in term and 'iphon' not in term and '5s' not in term and 'あいふぉん' not in term and 'アイフォン' not in term and 'アイフォーン' not in term and 'アイホン' not in term  and 'アイホーン' not in term:
     terms.append( term )
 
 open('terms.json', 'w').write( json.dumps(terms, indent=2, ensure_ascii=False) )
